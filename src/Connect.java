6190d7d14b5750ccb1bbe58f383445d55cc2f2af
Johann Grabner <jgrabner@business-tuning.at>
2023-08-23 07:24:14 +0200

    Connect to database

---
+++ src/Connect.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/LVs/DBP2023/Campus02JDBC.db";
+            //Wie ist ein connection-String aufgebaut - DriverName:Filename
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+<<<<<<< HEAD
         System.out.printf("Hello JDBC");
 
         // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
@@ -14,5 +15,10 @@ public class Main {
             // for you, but you can always add more by pressing Strg+F8.
             System.out.println("i = " + i);
         }
+=======
+
+        Connect.connect();
+
+>>>>>>> 9181940 (Connect to database)
     }
 }
