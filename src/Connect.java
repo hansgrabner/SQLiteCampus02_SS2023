57e73327969da641011d275beb23f058d0d89b5f
Johann Grabner <jgrabner@business-tuning.at>
2023-08-23 11:03:19 +0200

    displayBewertungenOrderByBewertung

--- src/Connect.java
+++ src/Connect.java
@@ -100,6 +100,48 @@ public class Connect {
         //1. Connection aufbauen
         //2. Resultset erzeugen
         //3. Ausgabe der Bewertungen inkl. Schlagwort aus der Tabelle Urlaube mit printf
+
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/LVs/DBP2023/Campus02JDBC.db";
+            //Wie ist ein connection-String aufgebaut - DriverName:Filename
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+            Statement stmt = conn.createStatement();
+
+            String query = "SELECT u.Schlagwort, b.Punkte, b.Kommentar\n" +
+                    "FROM Urlaube u JOIN Bewertungen b\n" +
+                    "ON u.UrlaubsID = b.UrlaubsId"+
+                    " ORDER BY Punkte DESC";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            //rs ist ein Zeiger auf eine virtuelle Tabelle,
+            // zeigt nach dem Öffnen auf den Datensatz - 1
+
+            while (rs.next()) {
+                System.out.printf("Schlagwort: %s Punkte: %d Kommentar %s %n",
+                        rs.getString("Schlagwort"),
+                        rs.getInt("Punkte"),
+                        rs.getString("Kommentar"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
     }
 
 
