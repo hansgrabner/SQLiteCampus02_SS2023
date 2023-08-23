1f5f6928be5c0de59c81ad6203bd9d38ed4687c5
Johann Grabner <jgrabner@business-tuning.at>
2023-08-23 07:31:37 +0200

    Connect to database

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,8 @@ public class Main {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-<<<<<<< HEAD
-        System.out.printf("Hello JDBC");
-
-        // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Strg+F8.
-            System.out.println("i = " + i);
-        }
-=======
 
         Connect.connect();
 
->>>>>>> 9181940 (Connect to database)
     }
 }
